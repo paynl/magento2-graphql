c8b3149dd7895cb4a230c1c084b5b73012c0325c
k.verschoor@pay.nl <k.verschoor@pay.nl>
2022-06-23 15:42:57 +0200

    Fix returntransfer

--- samples/paynlFinishTransaction.php
+++ samples/paynlFinishTransaction.php
@@ -30,7 +30,7 @@ $data = json_encode(['query' => $query, 'variables' => $variables]);
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
