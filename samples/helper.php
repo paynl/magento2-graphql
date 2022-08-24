7e71e7fb65725da059a69db08595f75f8acd1607
Wouter Jonker <wouter@pay.nl>
2022-08-24 14:50:27 +0200

    Update helper.php

--- samples/helper.php
+++ samples/helper.php
@@ -4,7 +4,7 @@ require_once('config.php');
 
 function getAdminToken()
 {
-    $urlRest = BASE_URL . "rest/all/V1/integration/admin/token?username=" . ADMIN_USERNAME . "&password=" . ADMIN_PASSWORD;
+    $urlRest = BASE_URL . "/rest/all/V1/integration/admin/token?username=" . ADMIN_USERNAME . "&password=" . ADMIN_PASSWORD;
 
     $headers = array();
     $headers[] = 'Content-Type: application/json';
